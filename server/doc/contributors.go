ef35a96a63b7cbbc0b2d7e1a75b35f08398b7562
Allen Sun <shlallen1990@gmail.com>
2018-01-18 09:50:53 +0800

    fix: add -c in exec
    Signed-off-by: Allen Sun <shlallen1990@gmail.com>

--- server/doc/contributors.go
+++ server/doc/contributors.go
@@ -6,12 +6,12 @@ import (
 )
 
 // generateContributors will generate file CONTRIBUTORS.
-// First, use newly built binary pouch to execute `pouch gen-doc` to generate Cli doc.
+// First, use newly built binary pouch to execute `generate-conrtibutors.sh` to generate Cli doc.
 // Second, git commit and push to github.
 // Third, use github to create a new pull request.
 func (g *Generator) generateContributors() error {
 	// auto generate cli docs
-	cmd := exec.Command("/bin/bash", "/go/src/github.com/alibaba/pouch/hack/generate-conrtibutors.sh")
+	cmd := exec.Command("/bin/bash", "-c", "/go/src/github.com/alibaba/pouch/hack/generate-conrtibutors.sh")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to generate contributors: %v", err)
 	}
