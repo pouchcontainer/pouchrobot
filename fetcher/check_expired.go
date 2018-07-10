0241ba865092c23f8b875906239746d4b165c685
沽酒 <>
2018-07-10 10:12:29 +0800

    添加注释

--- fetcher/check_expired.go
+++ fetcher/check_expired.go
@@ -11,14 +11,17 @@ import (
 	"github.com/pouchcontainer/pouchrobot/config"
 )
 
-const EXPIRETIME = 1 * utils.Minute
+const EXPIRETIME = 30 * utils.Day
 const BASEURL = "https://api.github.com/"
 
+//Processing expired issue
 func (f *Fetcher) CheckExpireIssue(config config.Config) error {
 
 	logrus.Info("start to check expire issue")
+	//initialization
 	opt := &github.IssueListByRepoOptions{}
 	issues := []*github.Issue{}
+	//Get issues list
 	issues,err := f.client.GetIssues(opt)
 
 	if err != nil {
@@ -27,11 +30,14 @@ func (f *Fetcher) CheckExpireIssue(config config.Config) error {
 	if len(issues) == 0 {
 		logrus.Info("there is no opened issue")
 	}
+	//Traversing issues
 	for _, issue := range issues {
 		logrus.Info("start to check issue #", issue.GetNumber())
+		//Calculation expired or not
 		deadline := issue.GetUpdatedAt().Add(EXPIRETIME)
 		current := time.Now()
 		diff := current.Unix() - deadline.Unix()
+		//if diff < 0 ,the issue is expired
 		if diff > 0 {
 			logrus.Info("issue #"+ strconv.Itoa(issue.GetNumber()) + " need to be closed")
 			CloseIssue(issue,f.client.Repo(),f.client.Owner(),config.AccessToken)
@@ -42,6 +48,11 @@ func (f *Fetcher) CheckExpireIssue(config config.Config) error {
 	return nil
 }
 
+//close by httprequest
+//method:PATCH
+//url:https://api.github.com/repos/:owner/:repo/issues/:issueNum
+//body:{"state":"closed"}
+//header: "Authorization":token,"Content-Type":"application/json"
 func CloseIssue(issue *github.Issue,repo,owner,token string) error{
 	jsonStr := []byte("{\"state\":\"closed\"}")
 	url := BASEURL + "repos/"+owner+"/"+repo+"/issues/" + strconv.Itoa(issue.GetNumber())
