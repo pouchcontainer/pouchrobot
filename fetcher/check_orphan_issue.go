337c588a7ec380cace1b76a7be6eccc9dc2b6928
zsx <zsx@ali-1c36bbeda387.local>
2018-07-14 13:38:51 +0800

    fix wrnog pacage defination

--- fetcher/check_orphan_issue.go
+++ fetcher/check_orphan_issue.go
@@ -10,17 +10,20 @@ import (
 func (f *Fetcher) CheckOrphanIssue() error {
 	logrus.Info("start to check orphan issues...")
 
+	now :=time.Now()
+	// TODO: configurable
+	var thirtyDayAgo = now.AddDate(0,0,-30)
 	opt := &github.IssueListByRepoOptions{
 		State: "open",
 		Sort: "comments",
 		Direction:"asc",
+		Since: thirtyDayAgo,
 	}
 	isues,err := f.client.GetIssues(opt)
 	if err != nil {
 		return err
 	}
 
-	logrus.Debugf("%v",isues)
 	// stop if all have comments
 	if len(isues) == 0 || *(isues[0].Comments) > 0{
 		logrus.Info("no orphan issue found...")
@@ -37,15 +40,14 @@ func (f *Fetcher) CheckOrphanIssue() error {
 
 // close specific Issue
 func (f *Fetcher) closeOrphanIssue(isue *github.Issue) error {
-	// check condition
-	now :=time.Now()
-	var thirtyDayAgo = now.AddDate(0,0,-30)
-	if *(isue.Comments) == 0  && (*isue.CreatedAt).After(thirtyDayAgo) {
+	// check comments
+	//var err error // why warn??
+	if *(isue.Comments) == 0 {
 		s := "closed"
 		parm := &github.IssueRequest{
 			State: &s,
 		}
-		_, err := f.client.EditIssue(parm, *(isue.Number))
+		_, err := f.client.EditIssue(parm, *(isue.ID))
 		return err
 	}
 	return nil
