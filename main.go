cbc4f48a8416c346d7ee3bcbdcd8564b664992df
Allen Sun <shlallen1990@gmail.com>
2018-10-25 22:23:58 +0800

    docs: update some cli details
    Signed-off-by: Allen Sun <shlallen1990@gmail.com>

--- main.go
+++ main.go
@@ -24,19 +24,21 @@ import (
 func main() {
 	var cfg config.Config
 	var cmdServe = &cobra.Command{
-		Use:  "An collaboration robot based on AI",
+		Use:  "A AI-based collaboration robot applied to open source project",
 		Args: cobra.MinimumNArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
 			s := NewServer(cfg)
 			logrus.Fatal(s.Run())
 		},
 	}
+	cmdServe.SilenceUsage = true
+	cmdServe.SilenceErrors = true
 
 	flagSet := cmdServe.Flags()
-	flagSet.StringVarP(&cfg.Owner, "owner", "o", "", "github ID to which connect in GitHub")
-	flagSet.StringVarP(&cfg.Repo, "repo", "r", "", "github repo to which connect in GitHub")
+	flagSet.StringVarP(&cfg.Owner, "owner", "o", "", "GitHub username to which this robot connects")
+	flagSet.StringVarP(&cfg.Repo, "repo", "r", "", "GitHub code repository to which this robot connects")
 	flagSet.StringVarP(&cfg.HTTPListen, "listen", "l", "", "where does robot listen on")
-	flagSet.StringVarP(&cfg.AccessToken, "token", "t", "", "access token to have some control on resources")
+	flagSet.StringVarP(&cfg.AccessToken, "token", "t", "", "access token which identifies robot username in GitHub having write access on repo")
 	flagSet.IntVarP(&cfg.CommitsGap, "commits-gap", "c", 20, "commits gap between pull request and master branch; if the fact is beyond this number, request to rebase")
 
 	// for weekly reporter
@@ -44,7 +46,7 @@ func main() {
 	flagSet.IntVar(&cfg.ReportHour, "report-hour", 7, "weekly report generation hour on report-day")
 
 	// for doc generator
-	flagSet.StringVar(&cfg.RootDir, "root-dir", "", "specifies repo's rootdir which is to generated docs")
+	flagSet.StringVar(&cfg.RootDir, "root-dir", "", "specifies repo's root directory which is to generated docs")
 	flagSet.StringVar(&cfg.SwaggerPath, "swagger-path", "", "specifies where the swagger.yml file locates")
 	flagSet.StringVar(&cfg.APIDocPath, "api-doc-path", "", "specifies where to generate the doc file corresponding to swagger.yml")
 
